4387084e98fac2c58ff720fa9bcdb313ce4f4298
Mohamed El Mahallawy <mmahalwy@gmail.com>
2014-08-18 20:55:15 -0400

    typo

--- lib/omniauth/strategies/zendesk.rb
+++ lib/omniauth/strategies/zendesk.rb
@@ -10,7 +10,7 @@ module OmniAuth
         :grant_type => 'authorization_code'
       }
       uid{
-        host = URI( .raw_info.user.url ).host.split(".").first
+        host = URI( raw_info.user.url ).host.split(".").first
       }
 
       extra do
